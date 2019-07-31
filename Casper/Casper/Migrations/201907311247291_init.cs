92b0913c37706a1fd212658abbc0a66958a930a0
nico57000 <falcettanicolas@gmail.com>
2019-07-31 14:49:28 +0200

    Creation projet + entity

---
+++ Casper/Casper/Migrations/201907311247291_init.cs
@@ -0,0 +1,38 @@
+namespace Alto_IT.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Projets",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nom = c.String(),
+                        CloudProvider = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Identifiant = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Projets");
+        }
+    }
+}
